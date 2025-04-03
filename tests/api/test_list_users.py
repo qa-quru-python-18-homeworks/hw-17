60cadfe37024dd8486dfe907fdeb6e840b295113
Waldemar G <vladdigen@icloud.com>
2025-04-03 14:54:32 +0400

    Update list users API tests and ignore IDEA files

--- tests/api/test_list_users.py
+++ tests/api/test_list_users.py
@@ -1,13 +1,28 @@
 import requests
 
 
-def test_endpoint_smoke(base_url, list_users_endpoint):
+def test_list_users_returns_200_on_valid_page_request(base_url, list_users_endpoint):
     response = requests.get(f"{base_url}{list_users_endpoint}", params={"page": 1})
     assert response.status_code == 200
 
 
-def test_that_page_has_six_users(base_url, list_users_endpoint):
+def test_list_users_returns_six_users_on_first_page(base_url, list_users_endpoint):
     response = requests.get(f"{base_url}{list_users_endpoint}", params={"page": 1})
     response_body = response.json()
     assert len(response_body["data"]) == 6
 
+
+def test_list_users_returns_first_page_by_default_without_params(base_url, list_users_endpoint):
+    response = requests.get(f"{base_url}{list_users_endpoint}")
+    response_body = response.json()
+    assert response_body["page"] == 1
+
+
+def test_list_users_returns_same_users_as_first_page_when_page_is_negative(base_url, list_users_endpoint):
+    response_page_one = requests.get(f"{base_url}{list_users_endpoint}", params={"page": 1})
+    response_body_page_one = response_page_one.json()
+
+    response_page_minus_one = requests.get(f"{base_url}{list_users_endpoint}", params={"page": -1})
+    response_body_page_minus_one = response_page_minus_one.json()
+
+    assert response_body_page_one["data"] == response_body_page_minus_one["data"]
